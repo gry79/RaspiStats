1b0eea4044d4adc9aa8b5dbf52cd7f2f03d5d653
Christophe Dirac <cdirac@me.com>
2020-06-21 18:25:08 +0200

    Added services-list

--- xinfo.sh
+++ xinfo.sh
@@ -12,21 +12,21 @@ HOSTNAME=`hostname -f`
 LOAD1=`cat /proc/loadavg | awk '{print $1}'`    # Letzte Minute
 LOAD2=`cat /proc/loadavg | awk '{print $2}'`    # Letzte 5 Minuten
 LOAD3=`cat /proc/loadavg | awk '{print $3}'`    # Letzte 15 Minuten
- 
+
 # Temperatur
 TEMP=`vcgencmd measure_temp | cut -c "6-9"`
- 
+
 # Speicherbelegung
 DISK1=`df -h | grep 'dev/root' | awk '{print $2}'`    # Gesamtspeicher
 DISK2=`df -h | grep 'dev/root' | awk '{print $3}'`    # Belegt
 DISK3=`df -h | grep 'dev/root' | awk '{print $4}'`    # Frei
- 
+
 # Arbeitsspeicher
 RAM1=`free -h -w | grep 'Mem' | awk '{print $2}'`    # Total
 RAM2=`free -h -w | grep 'Mem' | awk '{print $3}'`    # Used
 RAM3=`free -h -w | grep 'Mem' | awk '{print $4}'`    # Free
 RAM4=`free -h -w | grep 'Swap' | awk '{print $3}'`    # Swap used
- 
+
 # IP-Adressen ermitteln
 ETHERNET_IP=`ifconfig eth0 2>/dev/null | grep "inet " | awk '{print $2}'`
 WLAN_IP=`ifconfig wlan0 2>/dev/null | grep "inet " | awk '{print $2}'`
@@ -44,3 +44,9 @@ echo -e "\033[1;32m   .~~.   .~~.    \033[1;36m$DATUM
 \033[1;31m   '~ .~~~. ~'    \033[0;37m
 \033[1;31m       '~'        \033[0;37m
 \033[m"
+
+if [ -f "/etc/services-list" ]; then
+  cat /etc/services-list
+fi
+
+echo
